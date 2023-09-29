c20dd017a0df2df85b20ecb72dd7ef4e370a4c4d
Nikita Ermakov <neketa.erma13@gmail.com>
2023-09-29 09:51:38 +0300

    delete no work prime/ng

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -17,7 +17,6 @@ import { AccountFooterComponent } from './layout/account-footer.component';
 // tenants
 import { TenantChangeComponent } from './tenant/tenant-change.component';
 import { TenantChangeDialogComponent } from './tenant/tenant-change-dialog.component';
-import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   imports: [
@@ -29,7 +28,6 @@ import { CheckboxModule } from 'primeng/checkbox';
     ServiceProxyModule,
     AccountRoutingModule,
     ModalModule.forChild(),
-    CheckboxModule,
   ],
 
   declarations: [
